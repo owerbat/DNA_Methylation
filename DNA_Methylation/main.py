a3be1dab169db2976622d0b9ce1fbb735cec610b
owerbat <niktimak@gmail.com>
2018-09-09 23:13:14 +0300

    The first version is completed

--- DNA_Methylation/main.py
+++ DNA_Methylation/main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def get_ages():
-    file = open("D:\\Projects\\DNA Methylation\\methylation_data\\age.txt", 'r', encoding="utf-8")
+    file = open("..\\methylation_data\\age.txt", 'r', encoding="utf-8")
 
     ages = []
     while True:
@@ -13,33 +13,46 @@ def get_ages():
         ages.append(int(current_age))
 
     file.close()
-    print(ages)
-    print(len(ages))
+
     return ages
 
 
 def gene_data_read(file):
     gene = file.readline().split(" ")
     gene_name = gene.pop(0)
-    print(gene_name)
 
     for i in range(len(gene)):
         gene[i] = float(gene[i])
-    print(gene)
-    print(len(gene))
 
     return gene_name, gene
 
 
 def main():
-    file = open("D:\\Projects\\DNA Methylation\\methylation_data\\gene_data.txt", 'r', encoding="utf-8")
+    file = open("..\\methylation_data\\gene_data.txt", 'r', encoding="utf-8")
 
+    r_values = []
+    names = []
     x = get_ages()
-    name, y = gene_data_read(file)
-    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-    print("r_value = " + str(r_value))
+    while True:
+        name, y = gene_data_read(file)
+        if len(y) == 0:
+            break
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+        r_values.append(r_value)
+        names.append(name)
 
     file.close()
+
+    if len(names) != len(r_values):
+        print("Error!")
+    else:
+        table = list([r_values[i], names[i]] for i in range(len(names)))
+        table.sort(reverse=True)
+
+        result_file = open("Results.txt", 'w', encoding="utf-8")
+        for i in range(10):
+            result_file.write(str(i+1) + ') ' + str(table[i][1]) + ' - ' + str(table[i][0]) + '\n')
+        result_file.close()
 
 
 main()
