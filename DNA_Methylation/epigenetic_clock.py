8089b5b8711c8ec5bd2d0e8506f3bf38bfb6d10b
owerbat <niktimak@gmail.com>
2018-11-23 20:36:39 +0300

    A histogram for GSE87571 base added

--- DNA_Methylation/epigenetic_clock.py
+++ DNA_Methylation/epigenetic_clock.py
@@ -2,24 +2,40 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def get_age_histogram():
-    file = open('..\\methylation_data\\attributes GSE40279.txt', 'r', encoding='utf-8')
+def get_age_histogram(file_name):
+    file = open(file_name, 'r', encoding='utf-8')
+    if file_name == '..\\methylation_data\\attributes GSE40279.txt':
+        age_column_number = 2
+        sex_column_number = 3
+        title = 'GSE40279'
+        figure_number = 1
+    elif file_name == '..\\methylation_data\\attributes GSE87571.txt':
+        age_column_number = 3
+        sex_column_number = 2
+        title = 'GSE87571'
+        figure_number = 2
+    else:
+        print('File error')
+        exit(1)
+
     ages = []
     male_ages = []
     female_ages = []
     file.readline()
     for line in file:
-        age = int(line.split()[2])
+        age = int(line.split()[age_column_number])
         ages.append(age)
 
-        sex = line.split()[3]
+        sex = line.split()[sex_column_number]
         if sex == 'M':
             male_ages.append(age)
         else:
             female_ages.append(age)
 
+    plt.figure(figure_number)
     plt.xlabel('Age')
     plt.ylabel('Count')
+    plt.title(title)
     plt.hist(ages, bins=np.arange(min(ages)+0.5, max(ages), 1), density=True, histtype='step', color='black')
     plt.hist(male_ages, bins=np.arange(min(ages) + 0.5, max(ages), 1), density=True, histtype='step', color='blue')
     plt.hist(female_ages, bins=np.arange(min(ages) + 0.5, max(ages), 1), density=True, histtype='step', color='pink')
@@ -27,7 +43,8 @@ def get_age_histogram():
 
 
 def main():
-    get_age_histogram()
+    get_age_histogram('..\\methylation_data\\attributes GSE40279.txt')
+    get_age_histogram('..\\methylation_data\\attributes GSE87571.txt')
 
 
 main()
